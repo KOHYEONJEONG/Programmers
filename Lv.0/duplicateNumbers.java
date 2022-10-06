a8fb065fa6c3b5879029375863d48f40950c2a02
KOHYEONJEONG <52101148+KOHYEONJEONG@users.noreply.github.com>
2022-10-06 22:09:54 +0900

    Create duplicateNumbers.java
    Forder Test

---
+++ Lv.0/duplicateNumbers.java
@@ -0,0 +1,21 @@
+public class duplicateNumbers { //중복된 숫자 개수(0레벨)
+	public int solution(int[] array, int n) {
+        int answer = 0;
+        
+        for(int data : array) {
+        	if(data == n) {
+        		answer++;
+        	}
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+		duplicateNumbers d = new duplicateNumbers();
+		int[] array = {1, 1, 2, 3, 4, 5};
+		int result = d.solution(array, 1);
+		System.out.println(result);
+	}
+}
